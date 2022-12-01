d57097701c4841986e68601a339cdc572e6614c9
CWIN77 <cwstbp0426@gmail.com>
2022-12-01 17:21:57 +0900

    db 연결 방식 수정

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
 const mongoose = require("mongoose");
 const redis = require('redis');
 
-const connected = { mongodb: 0, redis: 0 };
+const connected = { mongo: null, redis: null };
 
-const connectDB = async (mongodbURI, redisInfo) => {
-  if (connected.mongodb !== 1) {
+const connectDB = async (mongoInfo, redisInfo) => {
+  if (!connected.mongo) {
     try {
-      const db = await mongoose.connect(mongodbURI);
-      connected.mongodb = db.connections[0].readyState;
-      console.log(`MongoDB Connected: ${db.connection.host}`);
+      await mongoose.connect(mongoInfo.uri);
+      connected.mongo = mongoose;
+      console.info('MongoDB Connected!');
     } catch (err) {
-      console.error("DB 연결 오류", err);
-      connected.mongodb = 0;
-      return err;
+      connected.mongo = null;
+      console.error('MongoDB Error');
     }
   }
 
-  if (connected.redis !== 1) {
+  if (!connected.redis) {
     const redisClient = redis.createClient({
       url: `redis://${redisInfo.username}:${redisInfo.password}@${redisInfo.host}:${redisInfo.port}/0`,
       legacyMode: true,
     });
     redisClient.on('connect', () => {
+      connected.redis = redisClient;
       console.info('Redis connected!');
-      connected.redis = 1;
     });
     redisClient.on('error', (err) => {
-      connected.redis = 0;
-      console.error('Redis Client Error', err);
+      connected.redis = null;
+      console.error('Redis Client Error');
     });
     await redisClient.connect().then();
-    const redisCli = redisClient.v4;
+    redisClient.v4;
   }
 }
 
+const setData = async (data) => {
+  if (connected.mongo && connected.redis) {
+    const redisCli = connected.redis.v4;
+    const redisData = await redisCli.get('data');
+    const { data } = JSON.parse(redisData);
+    data.push({ text: req.body.text });
+    await redisCli.set('data', JSON.stringify({ data }));
+  } else console.error("connectDB");
+}
 
 
-module.exports = { connectDB }
+module.exports = { connectDB, setData }
--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-const { connectDB } = require("./index");
+const { connectDB, setData } = require("./index");
 const dotenv = require('dotenv');
 dotenv.config();
-const { REDIS_HOST, REDIS_PORT, REDIS_USERNAME, REDIS_PASSWORD } = process.env;
-const redisInfo = { password: REDIS_PASSWORD, username: REDIS_USERNAME, port: REDIS_PORT, host: REDIS_HOST };
+const { REDIS_HOST, REDIS_PORT, REDIS_USERNAME, REDIS_PASSWORD, MONGODB_URI } = process.env;
 
-connectDB(process.env.MONGODB_URI, redisInfo);
+const testGoalSchema = ({
+  text: {
+    type: String,
+    required: [true, "please add a text value"]
+  }
+},
+{
+  timestamps: true
+});
+
+const redisInfo = { password: REDIS_PASSWORD, username: REDIS_USERNAME, port: REDIS_PORT, host: REDIS_HOST };
+const mongoInfo = { uri: MONGODB_URI, schema: testGoalSchema, schemaName: "Goal" };
+
+
+const start = async () => {
+  await connectDB(mongoInfo, redisInfo);
+  setData();
+}
+start();
